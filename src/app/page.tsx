2772337ca01df1b4efb75b3ded28e0a6932e28ec
BT-Matshazi <bekithembamatshazi@gmail.com>
2023-12-28 15:17:34 +0200

    button padding update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
           <div className="mt-6 flex flex-col gap-4 sm:flex-row">
             <Link
               href="/products"
-              className="inline-flex items-center justify-center whitespace-nowrap rounded-md bg-black px-5 text-sm font-medium text-white ring-offset-background transition-colors hover:bg-gray-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+              className="inline-flex items-center justify-center whitespace-nowrap rounded-md bg-black px-5 py-2 text-sm font-medium text-white ring-offset-background transition-colors hover:bg-gray-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
             >
               Browse Collection
             </Link>
