3459f6af74962239b0a8cca1d5a763dc14824bfd
BT-Matshazi <bekithembamatshazi@gmail.com>
2024-01-01 12:16:43 +0200

    sticky nav

--- src/app/common/Header.tsx
+++ src/app/common/Header.tsx
@@ -61,60 +61,61 @@ export default function NavBar() {
 
   return (
     <>
-      <div className="m-auto w-full border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-900">
-        <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-          <a
-            href="/"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
-          >
-            <Image src={Logo} className="m-0 w-32 p-0" alt="Logo" />
-          </a>
-          <div className="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
-            <Cart />
-            <button
-              type="button"
-              className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              onClick={toggleMobileMenu}
+      <div className="sticky inset-x-0 top-0 z-50 h-16 bg-white">
+        <div className="m-auto w-full border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-900">
+          <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
+            <a
+              href="/"
+              className="flex items-center space-x-3 rtl:space-x-reverse"
             >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="h-5 w-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
+              <Image src={Logo} className="m-0 w-32 p-0" alt="Logo" />
+            </a>
+            <div className="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
+              <Cart />
+              <button
+                type="button"
+                className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                onClick={toggleMobileMenu}
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
-          </div>
-          <div
-            className={`w-full items-center justify-between md:order-1 md:flex md:w-auto ${
-              mobileMenuOpen ? "block" : "hidden"
-            }`}
-            id="navbar-cta"
-          >
-            <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium hover:text-gray-500 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
-              {menuItems.map((item) => {
-                return (
-                  <li key={item.id}>
-                    <Link
-                      href={item.url}
-                      className=" block rounded px-3 py-2 text-xl font-bold text-gray-900 hover:bg-gray-100 sm:hover:text-gray-500 md:p-0 md:hover:bg-transparent dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    >
-                      {item.name}
-                    </Link>
-                  </li>
-                );
-              })}
+                <span className="sr-only">Open main menu</span>
+                <svg
+                  className="h-5 w-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 17 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 1h15M1 7h15M1 13h15"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div
+              className={`w-full items-center justify-between md:order-1 md:flex md:w-auto ${
+                mobileMenuOpen ? "block" : "hidden"
+              }`}
+              id="navbar-cta"
+            >
+              <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium hover:text-gray-500 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
+                {menuItems.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <Link
+                        href={item.url}
+                        className=" block rounded px-3 py-2 text-xl font-bold text-gray-900 hover:bg-gray-100 sm:hover:text-gray-500 md:p-0 md:hover:bg-transparent dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                      >
+                        {item.name}
+                      </Link>
+                    </li>
+                  );
+                })}
 
-              {/* <li aria-labelledby="dropdownNavbarLink">
+                {/* <li aria-labelledby="dropdownNavbarLink">
                 <button
                   id="doubleDropdownButton"
                   type="button"
@@ -165,7 +166,8 @@ export default function NavBar() {
                   </ul>
                 </div>
               </li> */}
-            </ul>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
